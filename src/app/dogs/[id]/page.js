efc19ac71d913689fd8510b0e0e0da923ba2cd45
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-05-24 10:49:05 -0400

    Add delete route

--- src/app/dogs/[id]/page.js
+++ src/app/dogs/[id]/page.js
@@ -1,9 +1,12 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Dog({ params }) {
   const [dog, setDog] = useState({})
+
+  const router = useRouter()
 
   useEffect(() => {
     const getDog = async () => {
@@ -18,10 +21,24 @@ export default function Dog({ params }) {
     getDog()
   }, [params.id])
 
+  const removeDog = async () => {
+    const response = await fetch(`https://arcane-bastion-92736.herokuapp.com/dogs/${params.id}`, {
+      headers: {'Content-Type': 'application/json'},
+      method: 'DELETE'
+    })
+    const adoptedDog = await response.json()
+
+    if(adoptedDog) {
+      router.push('/dogs')
+    }
+  }
+
   return (
     <div>
       <h1>{dog.name}</h1>
       <h1>{dog.age}</h1>
+
+      <button onClick={removeDog}>Remove Dog</button>
     </div>
   )
 }
